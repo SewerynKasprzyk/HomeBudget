a2a9801c29d5c51cb948dddb77962232535f2079
SewerynKasprzyk <sk302005@student.polsl.pl>
2023-11-25 22:11:07 +0100

    Setting up Login.

---
+++ HomeBudgetManagement/Background/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Background
+{
+    public class User
+    {
+        User(String name, String surname, String password)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.password = password;
+            this.accessLevel = 0; // 0 indicates user have guest access level
+        }
+
+        public string Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+        public string Surname { get => surname; set => surname = value; }
+        public string Password { get => password; set => password = value; }
+        public int AccessLevel { get => accessLevel; set => accessLevel = value; }
+
+        private String id;
+        private String name;
+        private String surname;
+        private String password;
+        private int accessLevel;
+    }
+}
---
+++ HomeBudgetManagement/Background/UserList.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Background
+{
+    public static class UserList
+    {
+        private static List<User> users;
+
+        public static List<User> Users { get => users; set => users = value; }
+
+        public static void addToList(User user)
+        { 
+            users.Add(user);
+        }
+
+        public static void removeFromList(String id)
+        {
+            foreach (User user in users)
+            {
+                if(user.Id == id) 
+                {
+                    users.Remove(user);
+                    return;
+                }
+            }
+    
+        }
+        
+    }
+}
