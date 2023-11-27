ddbd2428762d7f6841682524247447438b254c40
SewerynKasprzyk <sk302005@student.polsl.pl>
2023-11-28 00:33:52 +0100

    Revert "Account manager done."
    This reverts commit 8a48386052d8bc0ef6c9e0874b6037e49ec41f91.

--- HomeBudgetManagement/Background/AccountManager.cs
+++ HomeBudgetManagement/Background/AccountManager.cs
@@ -8,124 +8,12 @@ namespace Background
 {
     public class AccountManager
     {
+        private User user;
+
         public AccountManager(String userId)
         {
-            this.verificated = false;
-            this.userId = userId;
-            this.ManageAccount(this.userId);
+            user = UserList.FindUser(userId);
         }
 
-        public void ManageAccount(String userId)
-        {
-            this.account.AccountId = UserList.FindUser(userId).Id;
-
-            int index = 0;
-            foreach (var account in AccountList.Accounts)
-            {
-                if (account.AccountId == this.account.AccountId)
-                {
-                    this.account = account;
-                    this.verificated = true;
-                    this.index = index;
-                    return;
-                }
-
-                this.verificated = false;
-                index++;
-            }
-        }
-
-        public void ChangeName(String name)
-        {
-            this.ReloadAcc();
-            this.account.AccountName = name;
-            this.SaveAcc();
-        }
-
-        public void ChangeSurname(String surname)
-        {
-            this.ReloadAcc();
-            this.account.AccountSurname = surname;
-            this.SaveAcc();
-        }
-
-        public bool ChangePassword(String password)
-        {
-            if (Login.IsPassworCorrect(password))
-            {
-                UserList.Users[index].Password = password;
-                return true;
-            }
-
-            return false;
-        }
-
-        public void Delete()
-        {
-            AccountList.Accounts.RemoveAt(this.index);
-        }
-
-        public void GivePower()
-        {
-            UserList.Users[index].AccessLevel = 2;
-        }
-
-        //demo
-        public bool addUser(String login, String password, String name, String surname, int power, String demoUserId)
-        {
-            if (!Login.IsPassworCorrect(password))
-            { return false; }
-            else
-            {
-                Account acc = new Account(demoUserId);
-                acc.AccountName = name;
-                acc.AccountSurname = surname;
-
-                User user = new User();
-                user.Login = login;
-                user.Password = password;
-                user.Id = demoUserId;
-                user.AccessLevel = power;
-
-                AccountList.AddToList(acc);
-                UserList.AddToList(user);
-                return true;
-            }
-        }
-
-
-        public bool addUser(String login, String password, String name, String surname, int power)
-        {
-            if (!Login.IsPassworCorrect(password))
-            { return false; }
-            else
-            {
-                Account acc = new Account();
-                acc.AccountName = name;
-                acc.AccountSurname = surname;
-
-                User user = new User();
-                user.Login = login;
-                user.Password = password;
-                user.AccessLevel= power;
-
-                return true;
-            }
-        }
-
-        private void ReloadAcc()
-        {
-            this.account = AccountList.Accounts[index];
-        }
-
-        private void SaveAcc()
-        {
-            AccountList.Accounts[index] = account;
-        }
-
-        private String userId;
-        private int index = -1;
-        private Account account;
-        private bool verificated;
     }
 }
