b4eb63890f5e39cf9c13ef3a56876b73c4335bfc
Jakub Gawlik <jg302012@student.polsl.pl>
2023-11-28 01:37:17 +0100

    essa bam bam

---
+++ HomeBudgetManagement/Background/ExpensesIncomes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Background
+{
+    internal class ExpensesIncome //Nazwa do zmiany jbc
+    {
+        public string Name { get => name; set => name = value; }
+        public double Value { get => value; set => this.value = value; }
+
+        ExpensesIncome(String name, double value)
+        {
+            this.name = name;
+            this.value = value;
+        }
+
+        private double value;
+        private String name;
+
+    }
+}
---
+++ HomeBudgetManagement/Background/ExpensesIncomesList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Background
+{
+    internal class ExpensesIncomesList
+    {
+        internal static List<ExpensesIncome> ExpensesList { get => expensesList; set => expensesList = value; }
+        internal static List<ExpensesIncome> IncomesList { get => incomesList; set => incomesList = value; }
+
+        public static void AddToList(ExpensesIncome expInc)
+        {
+            if(expInc.Value > 0)
+            {
+                incomesList.Add(expInc);
+            }
+            else
+            {
+                expensesList.Add(expInc);
+            }  
+        }
+        
+        private static List<ExpensesIncome> expensesList;
+        private static List<ExpensesIncome> incomesList;
+    }
+}
