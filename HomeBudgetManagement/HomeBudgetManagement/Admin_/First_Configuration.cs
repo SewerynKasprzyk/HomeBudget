6aa1347be0999eb92403a9e7d0839face092aaa0
Kacper <rohhan.514@gmail.com>
2023-12-18 22:23:47 +0100

    First Config Form

--- HomeBudgetManagement/HomeBudgetManagement/Admin_/First_Configuration.cs
+++ HomeBudgetManagement/HomeBudgetManagement/Admin_/First_Configuration.cs
@@ -21,5 +21,80 @@ namespace HomeBudgetManagement.Admin_
         {
 
         }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void login_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void login_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxSetAdminLogin_click(object sender, EventArgs e)
+        {
+            Color customColor = Color.FromArgb(234, 243, 251);
+
+            textBoxSetAdminLogin.BackColor = Color.White;
+            panelSetAdminLogin.BackColor = Color.White;
+            panelSetAdminPassword.BackColor = customColor;
+            textBoxSetAdminPassword.BackColor =customColor;
+        }
+
+        private void textBoxSetAdminPassword_click(object sender, EventArgs e)
+        {
+            Color customColor = Color.FromArgb(234, 243, 251);
+
+            textBoxSetAdminLogin.BackColor = customColor;
+            panelSetAdminLogin.BackColor = customColor;
+            panelSetAdminPassword.BackColor = Color.White;
+            textBoxSetAdminPassword.BackColor = Color.White;
+        }
+
+        private void buttonClose_click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
     }
 }
--- HomeBudgetManagement/HomeBudgetManagement/Client.Designer.cs
+++ HomeBudgetManagement/HomeBudgetManagement/Client.Designer.cs
@@ -57,11 +57,11 @@
             // 
             this.textboxLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(243)))), ((int)(((byte)(251)))));
             this.textboxLogin.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textboxLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textboxLogin.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Bold);
             this.textboxLogin.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(132)))), ((int)(((byte)(219)))));
             this.textboxLogin.Location = new System.Drawing.Point(39, 15);
             this.textboxLogin.Name = "textboxLogin";
-            this.textboxLogin.Size = new System.Drawing.Size(443, 19);
+            this.textboxLogin.Size = new System.Drawing.Size(443, 20);
             this.textboxLogin.TabIndex = 2;
             this.textboxLogin.Tag = "";
             this.textboxLogin.Click += new System.EventHandler(this.textboxLogin_click);
@@ -74,7 +74,7 @@
             this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button1.FlatAppearance.BorderSize = 0;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Font = new System.Drawing.Font("Impact", 11.75F);
             this.button1.ForeColor = System.Drawing.Color.White;
             this.button1.Location = new System.Drawing.Point(318, 301);
             this.button1.Name = "button1";
@@ -88,12 +88,12 @@
             // 
             this.textBoxPassword.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(243)))), ((int)(((byte)(251)))));
             this.textBoxPassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBoxPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxPassword.Font = new System.Drawing.Font("Impact", 12F);
             this.textBoxPassword.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(132)))), ((int)(((byte)(219)))));
             this.textBoxPassword.Location = new System.Drawing.Point(39, 12);
             this.textBoxPassword.Name = "textBoxPassword";
             this.textBoxPassword.PasswordChar = '●';
-            this.textBoxPassword.Size = new System.Drawing.Size(443, 19);
+            this.textBoxPassword.Size = new System.Drawing.Size(443, 20);
             this.textBoxPassword.TabIndex = 8;
             this.textBoxPassword.Click += new System.EventHandler(this.texBoxPassword_click);
             this.textBoxPassword.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
@@ -150,34 +150,34 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label5.Font = new System.Drawing.Font("Impact", 16F);
             this.label5.ForeColor = System.Drawing.SystemColors.Window;
             this.label5.Location = new System.Drawing.Point(12, 254);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(86, 26);
+            this.label5.Size = new System.Drawing.Size(78, 27);
             this.label5.TabIndex = 2;
             this.label5.Text = "System";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label4.Font = new System.Drawing.Font("Impact", 16F);
             this.label4.ForeColor = System.Drawing.Color.White;
             this.label4.Location = new System.Drawing.Point(12, 219);
             this.label4.Name = "label4";
             this.label4.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.label4.Size = new System.Drawing.Size(279, 26);
+            this.label4.Size = new System.Drawing.Size(254, 27);
             this.label4.TabIndex = 1;
             this.label4.Text = "Home Budget Management\r\n";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label3.Font = new System.Drawing.Font("Impact", 16F);
             this.label3.ForeColor = System.Drawing.Color.WhiteSmoke;
             this.label3.Location = new System.Drawing.Point(12, 181);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(170, 26);
+            this.label3.Size = new System.Drawing.Size(156, 27);
             this.label3.TabIndex = 0;
             this.label3.Text = "Welcome to the ";
             this.label3.Click += new System.EventHandler(this.label3_Click_1);
@@ -197,7 +197,7 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::HomeBudgetManagement.Properties.Resources.icons8_user_24;
-            this.pictureBox1.Location = new System.Drawing.Point(9, 10);
+            this.pictureBox1.Location = new System.Drawing.Point(9, 11);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(24, 24);
             this.pictureBox1.TabIndex = 13;
@@ -247,7 +247,7 @@
             this.buttonForgetPassword.Cursor = System.Windows.Forms.Cursors.Hand;
             this.buttonForgetPassword.FlatAppearance.BorderSize = 0;
             this.buttonForgetPassword.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.buttonForgetPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonForgetPassword.Font = new System.Drawing.Font("Impact", 9.75F);
             this.buttonForgetPassword.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(132)))), ((int)(((byte)(219)))));
             this.buttonForgetPassword.Location = new System.Drawing.Point(459, 301);
             this.buttonForgetPassword.Name = "buttonForgetPassword";
@@ -259,12 +259,12 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21F);
+            this.label1.Font = new System.Drawing.Font("Impact", 21F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(132)))), ((int)(((byte)(219)))));
             this.label1.Location = new System.Drawing.Point(341, 142);
             this.label1.Name = "label1";
             this.label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.label1.Size = new System.Drawing.Size(285, 32);
+            this.label1.Size = new System.Drawing.Size(262, 35);
             this.label1.TabIndex = 15;
             this.label1.Text = "Login to your account";
             // 
